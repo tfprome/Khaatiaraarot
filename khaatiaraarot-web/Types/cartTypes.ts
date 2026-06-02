90c349d093fdfaf0459541d76cc1aa09d3a0d980
tfprome <tfpromy99@gmail.com>
2026-06-02 11:24:12 +0600

    added cart and logout feature

---
+++ khaatiaraarot-web/Types/cartTypes.ts
@@ -0,0 +1,19 @@
+export interface CartItem {
+    id: string;
+    name: string;
+    price: number;
+    originalPrice?: number;
+    quantity: number;
+    unit: string;
+    image: string;
+  }
+
+export interface CartContextValue {
+    items: CartItem[];
+    totalQty: number;
+    subtotal: number;
+    addItem: (item: Omit<CartItem, "quantity">) => void;
+    removeItem: (id: string) => void;
+    updateQuantity: (id: string, quantity: number) => void;
+    clearCart: () => void;
+  }
