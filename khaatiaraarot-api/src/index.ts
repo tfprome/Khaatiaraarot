a539f0a17af487399bfcf26580f51cfd331fecf6
Rowky411 <rowky4111@gmail.com>
2026-07-10 17:51:31 +0600

    swagger doc

--- khaatiaraarot-api/src/index.ts
+++ khaatiaraarot-api/src/index.ts
@@ -39,7 +39,11 @@ app.use("/api/v1/docs",
   helmet({ contentSecurityPolicy: false }),
   (req: express.Request, _res: express.Response, next: express.NextFunction) => { req.headers['x-forwarded-proto'] = 'http'; next(); },
   swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec)
+  swaggerUi.setup(swaggerSpec, {
+    swaggerOptions: {
+      oauth2RedirectUrl: `http://${process.env.HOST_URL || '109.199.110.47:4050'}/api/v1/docs/oauth2-redirect.html`,
+    },
+  })
 );
 
 app.use("/api/v1/auth", authLimiter, authRoutes);
